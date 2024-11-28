ccab21c34a2b0e24a218d5bbc837307e999c0a93
Sujoy De <sujoydeofficial@gmail.com>
2024-11-28 21:45:33 +0530

    fix: prod bugs

--- backend/restro_backend/urls.py
+++ backend/restro_backend/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("core.urls")),
-]
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT})
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,7 @@
-echo "Generating Secret..."
-export SECRET_KEY=$(openssl rand -hex 32)
 echo "Migrating database..."
 python manage.py migrate
+echo "Collecting static files..."
+python manage.py collectstatic
 echo "Loading fixtures..."
 python manage.py loaddata fixtures/categories.json
 python manage.py loaddata fixtures/food-items.json
